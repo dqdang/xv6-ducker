a998702979a7724564a107e90cb782e9a9d35ba2
Derek Dang <dqdang17@gmail.com>
2023-07-08 21:49:43 -0700

    Remove process in print

--- container.c
+++ container.c
@@ -1,18 +1,13 @@
 #include "types.h"
 #include "defs.h"
-#include "param.h"
-#include "mmu.h"
-#include "x86.h"
-#include "proc.h"
 #include "container.h"
 
 int create_container(int selected_console) {
     struct container current_container = {0};
 
     current_container.container_id = selected_console;
-    struct proc *proc = myproc();
 
-    cprintf("selected_console: %d, cid: %d, pid: %d\n", selected_console, current_container.container_id, &proc->pid);
+    cprintf("selected_console: %d, cid: %d\n", selected_console, current_container.container_id);
 
     return 23;
 }
